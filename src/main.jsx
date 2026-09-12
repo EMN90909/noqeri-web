44aec9810c0b018d7463bb9bddbf7e2a2679be77
EMN <nasongoemmanuel8@gmail.com>
2026-09-12 21:22:05 +0300

    Wire the Noqeri motion system into the app

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { NoqeriMotionProvider } from './motion/MotionProvider.jsx'
 import './styles.css'
+import './motion.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <NoqeriMotionProvider>
+        <App />
+      </NoqeriMotionProvider>
     </BrowserRouter>
   </React.StrictMode>
 )
