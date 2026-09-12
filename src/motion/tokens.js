34648e93fb3315c51af5eec0a5e993bf293f240f
EMN <nasongoemmanuel8@gmail.com>
2026-09-12 21:21:41 +0300

    Add centralized Noqeri motion tokens

---
+++ src/motion/tokens.js
@@ -0,0 +1,7 @@
+export const motionTokens={
+  duration:{instant:.1,micro:.16,fast:.26,base:.46,slow:.68,hero:.9},
+  ease:{sharp:[.22,1,.36,1],editorial:[.16,1,.3,1],symmetric:[.65,0,.35,1]},
+  distance:{micro:3,component:9,section:24},
+  stagger:{label:.028,nav:.036,hero:.075,card:.055},
+  spring:{type:'spring',stiffness:360,damping:32,mass:.7}
+}
